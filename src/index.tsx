f787a1dd51895ea820a55ef2349281e293261a67
Roman Zavalii <roman.zavalii@joblocal.de>
2024-07-30 16:50:19 +0200

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
+import {useEffect, useRef, useState} from 'react';
+import * as esbuild from 'esbuild-wasm';
+import {unpkgPathPlugin} from './plugins/unpkg-path-plugin';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+  const serviceRef = useRef<any>();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const startService = async () => {
+    serviceRef.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    })
+  }
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!serviceRef.current) return;
+
+    const result = await serviceRef.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      }
+    })
+
+    setCode(result.outputFiles[0].text);
+  }
+
+  return (
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  )
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
